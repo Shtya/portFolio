16d622d35195f355a9c2d8a926f185a11fcb130c
Shtya <Shtya54@gmail.com>
2023-03-17 18:55:08 +0200

    four

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -20,23 +20,31 @@ const Home = () => {
     <div className='HOME '>
 
     <section className="home" id="home">
-      <div className="cont">
-        <div className="shap">
-          <span className="line"></span>
-          <span className="left"></span>
-          <span className="right"></span>
-          <img src={avatar} alt="" />
-        </div>
-        {/* <h2 data-strok="Ahmed ShTYa">Ahmed ShTYa</h2> */}
-        <ul>
+        <div className="shape">
+          <span className="line_1">
+          <span className="bullet_4"></span>
+          <span className="bullet_5"></span>
+            <span className="bullet_6"></span>
+            <ul>
           <li><a target="_blank" href="https://www.facebook.com/profile.php?id=100008364883535"><i className="fa-brands fa-facebook"></i></a></li>
           <li><a target="_blank" href="https://mail.google.com/mail/u/0/#inbox"><i className="fa-solid fa-envelope"></i></a></li>
           <li><a target="_blank" href="https://wa.me/01551495772"><i className="fa-brands fa-whatsapp"></i></a></li>
           <li><a target="_blank" href="https://github.com/Shtya"><i className="fa-brands fa-github"></i></a></li>
           <li><a target="_blank" href="https://www.linkedin.com/in/ahmed-abdelrhman-78bb18230/"><i className="fa-brands fa-linkedin"></i></a></li>
-        </ul>
-        <a className='Download' href="AhmedShtya.pdf" download>Download CV</a>
+            </ul>
+          </span>
+          <span className="line_2">
+          <span className="bullet_1"></span>
+          <span className="bullet_2"></span>
+            <span className="bullet_3"></span>
+            <a className='Download' href="AhmedShtya.pdf" download>Download CV</a>
+
+          </span>
+          
+          
         </div>
+          <div className="cover"> <div className="innerCover"> <img src={avatar} alt="" /></div> </div>
+
       </section>
       
 
